50998774eb4c2fcd4b721e1a00e532caae260f11
chaeyoung moon <cymoon05@naver.com>
2022-04-25 20:51:58 +0900

    말이 보드판을 따라 이동

--- main.cpp
+++ main.cpp
@@ -9,12 +9,17 @@ enum GAME_MENU {
 	MENU_EXIT
 };
 
+enum GAME_SIZE {
+	WIDTH_SIZE = 7, 
+	HEIGHT_SIZE = 7
+};
+
 enum SPECIAL_MAP {
 	STAR_MAP1 = 5,
 	DE_ISRAND,
 	STAR_MAP2 = 21,
 	STAR_MAP3 = 34,
-	END_MAP = 48
+	END_MAP = 7
 };
 
 using namespace std;
@@ -33,35 +38,50 @@ using namespace std;
 //게임은 상대방이 파산하거나 100만원을 먼저 모으면 승리
 
 struct _tagPlayer {
+	bool ifTwo = false; // 한 번 이상 시작지점을 돌았는지
 	char strName[NAME_SIZE];
-	int iPosition = 1;
+	int iPosition = 0;
+	int iPos = 0;
 	string card;
 	int money = 10000;
 };
 
 int main() {
-
 	srand((unsigned int)time(0));
 
 	_tagPlayer tPlayer = {};
 	_tagPlayer tAI = {};
 	strcpy(tAI.strName, "Computer");
 
+	
+	// 칸 위치 집어넣기
+	int mapPosition[WIDTH_SIZE*HEIGHT_SIZE] = {};
+	int po = 0;
+	for (int i = 0; i <= WIDTH_SIZE - 1; i++)
+		mapPosition[po++] = i;
+	for (int i = 1; i <= HEIGHT_SIZE - 1; i++)
+		mapPosition[po++] = i * HEIGHT_SIZE + (HEIGHT_SIZE-1);
+	for (int i = (WIDTH_SIZE - 2); i >= 0; i--)
+		mapPosition[po++] = (WIDTH_SIZE-1) * WIDTH_SIZE + i;
+	for (int i = HEIGHT_SIZE - 2; i > 0; i--)
+		mapPosition[po++] = i * HEIGHT_SIZE;
+
 	cout << "이름을 입력하세요 : ";
 	cin >> tPlayer.strName;
 	while (true) {
+		char gamesize[WIDTH_SIZE][HEIGHT_SIZE] = {};
 		system("cls");
 		printf("==== 부유마불 ====\n\n");
 		for (int i = 0; i < 7; i++) {
 			for (int j = 0; j < 7; j++) {
 				int a = i * 7 + j;
-				if (a == tPlayer.iPosition - 1)
+				if (a == tPlayer.iPosition)
 					cout << "§";
-				else if (a == tAI.iPosition - 1)
+				else if (a == tAI.iPosition)
 					cout << "＆";
 				else if (a == DE_ISRAND)
 					cout << "▤";
-				else if (a == STAR_MAP1 ||a == STAR_MAP2 || a == STAR_MAP3)
+				else if (a == STAR_MAP1 || a == STAR_MAP2 || a == STAR_MAP3)
 					cout << "☆";
 				else if (!i || i == 6 || !j || j == 6)
 					cout << "□";
@@ -71,8 +91,11 @@ int main() {
 			cout << endl;
 		}
 
-		cout << endl << tPlayer.strName << " 말 위치 : " << tPlayer.iPosition <<endl;
-		cout << tAI.strName << " 말 위치 : " << tAI.iPosition << endl <<endl; 
+		
+
+		// 말 위치 출력
+		cout << endl << tPlayer.strName << " 말 위치 : " << mapPosition[tPlayer.iPos] <<endl;
+		cout << tAI.strName << " 말 위치 : " << mapPosition[tAI.iPos] << endl <<endl;
 		
 		cout << "1. 주사위 굴리기" << endl;
 		cout << "2. 게임 종료" << endl;
@@ -83,23 +106,21 @@ int main() {
 		switch(iMenu) {
 		case MENU_ROLL :
 			// 플레이어 이동
-			int iDice1 = rand() % 6 + 1;
-			int iDice2 = rand() % 6 + 1;
-			int sumDice = iDice1 + iDice2;
-			cout << tPlayer.strName << "(이)가 주사위 " << iDice1 << ", " << iDice2 << "을 던졌습니다! " << sumDice << "칸을 이동합니다." << endl;
-			tPlayer.iPosition += sumDice;
+			int dice = rand() % 6 + 1;
+			cout << tPlayer.strName << "(이)가 주사위 " << dice<< "을 던졌습니다! " << dice << "칸을 이동합니다." << endl;
+			tPlayer.iPos += dice;	// 9
+			
+			tPlayer.iPosition = mapPosition[tPlayer.iPos];
 
 			// 컴퓨터 이동
-			iDice1 = rand() % 6 + 1;
-			iDice2 = rand() % 6 + 1;
-			sumDice = iDice1 + iDice2;
-			cout << tAI.strName << "(이)가 주사위 " << iDice1 << ", " << iDice2 << "을 던졌습니다! " << sumDice << "칸을 이동합니다." << endl;
-			tAI.iPosition += sumDice;
+			dice = rand() % 6 + 1;
+			cout << tAI.strName << "(이)가 주사위 " << dice << "을 던졌습니다! " << dice << "칸을 이동합니다." << endl;
+			tAI.iPos += dice;
+			tAI.iPosition = mapPosition[tAI.iPos];
 
-			// 끝에 도달 시 다시 돌아감
-			if (tPlayer.iPosition >= END_MAP) tPlayer.iPosition = 1;
-			if (tAI.iPosition >= END_MAP) tAI.iPosition = 1;
-
+			// 만약 끝에 도달하면 다시 처음으로 돌아간다.
+				/*if (END_MAP) tPlayer.iPosition = 0, tPlayer.iPos = 0;
+				if (END_MAP) tAI.iPosition = 0, tAI.iPos = 0;*/
 			getchar();
 			break;
 		}
